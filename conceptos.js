fdec216eefe542058cf0a51f94d02485df40922d
Jonatan Serna <jonatansernahiguera@gmail.com>
2024-01-03 14:40:11 -0500

    none

---
+++ conceptos.js
@@ -0,0 +1,6 @@
+//Event Loop: El bucle de eventos es un patron de diseno que espera y distribuye eventos o mensajes en un programa.....Tarea asignada para mover del Task Queue al Stack, solo si el stack esta vacio
+//Memory Heap: Los objetos son asignados a un monticulo (espacio grande en memoria no organizado), se visualiza como un espacio en donde se almacenan datos sin un orden
+//Call Stack (Pila): Apila de forma organizada las instrucciones de un programa, se ve el concepto de LIFO (Last In, First Out), .push(), .pop()
+//Task Queue: Cola de tareas se manaeja la concurrencia, se agregan las tareas que ya estan listas, para pasar al Stack (pila), el stack debe estar vacio
+//MicroTask Queue: Promesas que tiene otra forma de ejecutarse y una prioridad superior
+//Web APIs: JS del lado del cliente, setTimeout, XMLHttpRequest, File Reader, DOM; Luego al nacer Node nos permite ir mas alla potenciandolo

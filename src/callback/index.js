fb134ea8d41703a49173066f4b1e12ca6a0d93ae
Jonatan Serna <jonatansernahiguera@gmail.com>
2024-01-04 15:40:39 -0500

    Promise

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,19 +1,3 @@
-function sum(num1, num2){
-    return num1 + num2;
-}
-
-function calc(num1, num2, callback){
-    return callback(num1, num2);
-};
-
-console.log(calc(2,2,sum))
-
-setTimeout(function (){
-    console.log("Hola JavaScript");
-},500)
-
-function gretting(name){
-    console.log(`Hola ${name}`);
-}
-
-setTimeout(gretting, 2000, "Jonatan")
+const promise = new Promise(function (resolve, reject) {
+    resolve ('Hey!')
+})
